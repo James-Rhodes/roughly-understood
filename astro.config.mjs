4bfd440c26ad0c619ec26460d90ad67d7d7318b2
James Rhodes <james.rhodes@hotmail.com.au>
2024-03-23 00:59:15 +1100

    Got the styling _close_ to what I want

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,10 @@
 import { defineConfig } from "astro/config";
 
+import tailwind from "@astrojs/tailwind";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://example.com",
+  integrations: [tailwind()],
 });
+
---
+++ tailwind.config.mjs
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {},
+    colors: {
+      "primary-bg": "#022b3a",
+      secondary: "#1f7a8c",
+      footer: "#011D27",
+      highlight: "#ffa62b",
+    },
+  },
+  plugins: [],
+};
